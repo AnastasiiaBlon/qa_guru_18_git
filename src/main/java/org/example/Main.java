d5a9439d09b0be6f43c64de1032a0336f096b954
Anastasiia <135753727+AnastasiiaBlon@users.noreply.github.com>
2024-11-24 13:47:41 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,8 @@
 package org.example;
 
 public class Main {
+    private static String msg = """Hello ga.guru!!!!!!!!!!!";
     public static void main(String[] args) {
-        System.out.println("Hello ga.guru!!!!!!!!!!!");
+        System.out.println(msg);
     }
 }
